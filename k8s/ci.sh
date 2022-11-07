3d685a4e64354066151aadb3131ca65631342092
emily <tom.niu8@gmail.com>
2022-11-08 07:57:22 +1100

    init

--- k8s/ci.sh
+++ k8s/ci.sh
@@ -1,3 +1,5 @@
+#!/bin/bash
+
 echo -n ${AWS_ACCESS_KEY_ID_VAL} > overlays/dev/AWS_ACCESS_KEY_ID.txt
 echo -n ${AWS_SECRET_ACCESS_KEY_VAL} > overlays/dev/AWS_SECRET_ACCESS_KEY.txt
 echo -n ${AWS_DEFAULT_REGION_VAL} > overlays/dev/AWS_DEFAULT_REGION.txt
