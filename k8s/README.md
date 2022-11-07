516391db0db6ca2f10ff742f9e03866bbaeb4659
emily <tom.niu8@gmail.com>
2022-11-08 07:40:47 +1100

    init

---
+++ k8s/README.md
@@ -0,0 +1,1 @@
+https://kubernetes.io/docs/tasks/configmap-secret/managing-secret-using-kustomize/
---
+++ k8s/ci.sh
@@ -0,0 +1,10 @@
+echo -n ${AWS_ACCESS_KEY_ID_VAL} > overlays/dev/AWS_ACCESS_KEY_ID.txt
+echo -n ${AWS_SECRET_ACCESS_KEY_VAL} > overlays/dev/AWS_SECRET_ACCESS_KEY.txt
+echo -n ${AWS_DEFAULT_REGION_VAL} > overlays/dev/AWS_DEFAULT_REGION.txt
+
+kubectl delete -k overlays/dev
+kubectl apply -k overlays/dev
+
+rm overlays/dev/AWS_ACCESS_KEY_ID.txt
+rm overlays/dev/AWS_SECRET_ACCESS_KEY.txt
+rm overlays/dev/AWS_DEFAULT_REGION.txt
